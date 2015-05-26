55f0a4448c2cf972b3ce696371631d9d8be32c5f
ambragg <sippinwine69@gmail.com>
2015-05-26 17:37:20 -0400

    Sweetr

--- Requests.swift
+++ Requests.swift
@@ -18,6 +18,8 @@ class FourSquareRequest {
     
     let CLIENT_SECRET = "42UGJMB3OJT4QKFFHDQO1EGCDOXPIH0WMO13WO4WIV5C4D2N"
     
+
+    
     func getVenuesWithLocation(location: CLLocation, completion: (venues: [AnyObject])  -> ()) {
         
         // venues empty array
@@ -34,21 +36,119 @@ class FourSquareRequest {
 class TwitterRequest {
     
     let API_URL = "https://api.twitter.com/1.1/"
+    let API_KEY = "pRMGj9TdXf1XwqDJQNgZIuQIq"
+    let API_SECRET = "YZambuvK7TSnDjn7B0JWXIeAoRz5xTSSfgNPHSefXL7MF9MgJ0"
     
-    func findPopularFoodTweets(completion: (tweets: [AnyObject]) -> ()) {
+    var API_ENCODED: String? {
         
-        let endpoint = API_URL + "search/tweets.json?q=Food&result_type=popular"
+        get {
+            
+            let str = API_KEY + ":" + API_SECRET
+            
+            let utf8str = str.dataUsingEncoding(NSUTF8StringEncoding)
+            
+            if let encoded = utf8str?.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.allZeros) {
+                
+                return "Basic " + encoded
+            }
+            
+            return nil
+            
+        }
+    }
+    
+    var TOKEN: String? {
+        
+        get {
+            
+            return NSUserDefaults.standardUserDefaults().objectForKey("twitterToken") as? String
+        }
+        
+            set {
+                
+                NSUserDefaults.standardUserDefaults().setObject(newValue, forKey: "twitterToken")
+                NSUserDefaults.standardUserDefaults().synchronize()
+        }
+    }
+    
+    func getToken(completion: () -> ()) {
+        
+        let endpoint = "https://api.twitter.com/oauth2/token?grant_type=client_credentials"
         
         if let url = NSURL(string: endpoint) {
-            let request = NSURLRequest(URL: url)
+            
+            var request = NSMutableURLRequest(URL: url)
+            
+            request.HTTPMethod = "POST"
+            
+            request.setValue(API_ENCODED, forHTTPHeaderField: "Authorization")
+            
+            request.setValue("application/x-www-form-urlencoded;charset=UTF-8", forHTTPHeaderField: "Content-Type")
             
             NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: { (response, data, error) -> Void in
                 
                 var error: NSError? = nil
                 
-                if let tweets = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &error) as? [AnyObject] {
+                if let returnInfo = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &error) as? [String:AnyObject] {
                     
-                    completion(tweets: tweets)
+                    println(returnInfo)
+                    
+                    self.TOKEN = returnInfo["access_token"] as? String
+                    
+                    completion()
+                    
+                }
+                
+                if let error = error {
+                    
+                    println(error)
+                }
+   
+                
+                
+            })
+            
+        }
+        
+        
+    }
+    
+    func findPopularFoodTweets(completion: (tweets: [AnyObject]) -> ()) {
+        
+        if TOKEN == nil {
+            
+            getToken({ () -> () in
+                
+                self.findPopularFoodTweets(completion)
+                
+            })
+            
+            return
+            
+        }
+        
+        let endpoint = API_URL + "search/tweets.json?q=Food&result_type=popular"
+        
+        if let url = NSURL(string: endpoint) {
+            
+            let request = NSMutableURLRequest(URL: url)
+            
+            request.setValue("Bearer " + TOKEN!, forHTTPHeaderField: "Authorization")
+            
+            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: { (response, data, error) -> Void in
+                
+                var error: NSError? = nil
+                
+                if let searchInfo = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &error) as? [String: AnyObject] {
+                    
+                    
+                    if let tweets = searchInfo["statuses"] as? [AnyObject] {
+                        
+                         completion(tweets: tweets)
+                        
+                        
+                    }
+
                     
                 }
                 
